b309ccac1afd0f1a4d4f3df7ff8004b29dae6d2d
duukkis <duukkis@hotmail.com>
2022-08-16 15:28:13 +0300

    tests matching

--- tests/test.php
+++ tests/test.php
@@ -13,6 +13,7 @@ for ($i = 1;$i <= $numberOfLogs;$i++) {
   if (file_exists("./input/log" . $i)) { 
     $result = Builder::buildResult("./input/log" . $i, $baseFormArray);
     file_put_contents("./output/result" . $i . ".txt", $result);
+    print $i . PHP_EOL;
   }
 }
 
